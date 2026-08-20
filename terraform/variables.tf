6b48b311ded9216fea9120c0de0c7bec6fe94019
filipburger <burgerfilip@gmail.com>
2026-08-20 19:29:22 +0200

    Apply terraform fmt formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 # terraform/variables.tf
-variable "project_id"       { default = "de-zoomcamp-499310" }
-variable "region"           { default = "us-central1" }
-variable "bucket_name"      { default = "filipburger-flight-data-lake" }
+variable "project_id" { default = "de-zoomcamp-499310" }
+variable "region" { default = "us-central1" }
+variable "bucket_name" { default = "filipburger-flight-data-lake" }
 variable "credentials_path" { default = "keys/terraform-runner.json" }
